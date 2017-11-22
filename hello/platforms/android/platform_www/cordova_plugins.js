51e363deb8d84dd322048699ce680d5abeadb623
Rupesh Chavan <rupesh@mindseed.in>
2017-11-22 17:15:57 +0530

    new plugin added

--- hello/platforms/android/platform_www/cordova_plugins.js
+++ hello/platforms/android/platform_www/cordova_plugins.js
@@ -1,11 +1,12 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "id": "cordova-plugin-splashscreen.SplashScreen",
-        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-        "pluginId": "cordova-plugin-splashscreen",
-        "clobbers": [
-            "navigator.splashscreen"
+        "id": "cordova-plugin-vibration.notification",
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "pluginId": "cordova-plugin-vibration",
+        "merges": [
+            "navigator.notification",
+            "navigator"
         ]
     }
 ];
@@ -13,7 +14,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-splashscreen": "4.1.1-dev"
+    "cordova-plugin-vibration": "2.1.6"
 };
 // BOTTOM OF METADATA
 });
